a8044de437dc1b9f6a4fa88e2357c7e0099c2ffc
Yanan Wu <ywu139@student.holmesglen.edu.au>
2022-07-26 16:20:09 +1000

    Tutorial2_Activity1

--- Tutorial2/Program.cs
+++ Tutorial2/Program.cs
@@ -6,7 +6,58 @@ namespace Tutorial2
     {
         static void Main(string[] args)
         {
-           
+            /*Class activity 1
+            Two people are playing the rock paper scissors game, build a program for the game. 
+            You may use numbers to represent the “rock”, “paper”, “scissors” states.
+            E.g. 1 - rock, 2 - paper, 3 - scissors */
+
+            Console.WriteLine("1 - rock, 2 - paper, 3 - scissors");
+            Console.WriteLine("playerA:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("playerB:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            string result;
+            if (A == B)
+            {
+                result = "Tie";
+            }
+            else if (A == 1)
+            {
+                if (B == 2)
+                {
+                    result = "B wins";
+                }
+                else
+                {
+                    result = "A wins";
+                }
+            }
+            else if (A == 2)
+            {
+                if (B == 1)
+                {
+                    result = "A wins";
+                }
+                else
+                {
+                    result = "B wins";
+                }
+
+            }
+            else
+            {
+                if (B == 1)
+                {
+                    result = "B wins";
+                }
+                else
+                {
+                    result = "A wins";
+                }
+            }
+            Console.WriteLine(result);
         }
+
     }
+
 }
